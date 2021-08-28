c32eb04b6047d49e90502088750ff4fe5d60325f
Adith Krishnan R <64576728+adithkrishnan98@users.noreply.github.com>
2021-08-28 10:41:14 +0530

    Update and rename RelativeSorting.java to 7. RelativeSorting.java

--- Arrays/7. RelativeSorting.java
+++ Arrays/7. RelativeSorting.java
@@ -1,4 +1,26 @@
-package Problems.Arrays;
+/* 
+Question :
+Given two arrays A1[] and A2[] of size N and M respectively. The task is to sort A1 in such a way that the relative order among the elements will be same as 
+those in A2. For the elements not present in A2, append them at last in sorted order. It is also given that the number of elements in A2[] are smaller than or 
+equal to number of elements in A1[] andA2[] has all distinct elements.
+
+Expected time complexity is O(N log(N)).
+
+Length of arrays N and M and next two line contains N and M elements respectively.
+
+Output:
+Print the relatively sorted array.
+
+Constraints:
+1 ≤ A1[], A2[] <= 10^6
+
+Explanation :
+We can use hashing technique to solve this problem. We first sort the arr1 using Arrays.sort(). Next, we create a hashMap with keys as elements of the arr1 
+and values as their counts. We loop through arr1 and store count of every number in the hashMap. Next, we loop through arr2 and check if the element is present 
+in hashmap, if it is then we put it in the output array that many number fo times and remove the element from the hashMap. Finally, we loop through the arr1 
+again and add the rest of the elements to the end of the output array in sorted order and we print the output array. 
+
+*/
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -47,8 +69,7 @@ public class RelativeSorting {
         }
 
     }
-
-    // Driver Code
+    
     public static void main(String[] args) {
         RelativeSorting sort = new RelativeSorting();
         int [] arr1 = {2,1,2,5,7,1,9,3,6,8,8};
@@ -64,3 +85,16 @@ public class RelativeSorting {
         sort.sortRelatively(arr3, arr4);
     }
 }
+
+/* 
+Output :
+2 2 1 1 8 8 3 5 6 7 9
+2 2 6 6 4 5 7 8
+
+Time Complexity : O(nLogn). The Arrays.sort() method takes O(nLogn) to sort the array. The rest of the for loops each take O(n) time individually so a total of 
+all of them would still be O(n). So we have O(nLogn + n) time complexity, since the former is the larger term the overall time complexity would be O(nLogn).
+Space Complexity : O(n) as we use extra space to create hash map and output array. 
+
+Video Explanation : https://drive.google.com/file/d/1yh61hrBBfNwgCPQCdBbBVIdj2csFzw4r/view?usp=sharing
+
+*/
