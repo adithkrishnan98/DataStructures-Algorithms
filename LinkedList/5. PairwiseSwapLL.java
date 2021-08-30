b20fa867048713b15757442baf973cc0aeedfe16
Adith Krishnan R <64576728+adithkrishnan98@users.noreply.github.com>
2021-08-30 12:11:52 +0530

    Update and rename PairwiseSwapLL.java to 5. PairwiseSwapLL.java

--- LinkedList/5. PairwiseSwapLL.java
+++ LinkedList/5. PairwiseSwapLL.java
@@ -1,4 +1,19 @@
-package Problems.LinkedList;
+/* 
+Question :
+Given a singly linked list of size N. The task is to swap elements in the linked list pairwise. For example, if the input list is 1 2 3 4, the resulting list 
+after swaps will be 2 1 4 3. Complete the function pairWiseSwap() which takes the the linked list and retu rns the modified linked list after swapping pairwise 
+nodes.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= N <= 103
+
+Explanation : 
+We start from the head node and traverse the list. While traversing we swap data of each node with its next node’s data. 
+*/
+
 
 public class PairwiseSwapLL {
     static Node head;
@@ -53,6 +68,16 @@ public class PairwiseSwapLL {
         System.out.println();
         ps.printList(head);
     }
-
-
 }
+
+/*
+Output :
+1-->2-->3-->4-->NULL
+2-->1-->4-->3-->NULL
+
+Time Complexity : O(n) as we traverse through n nodes of a linked list 
+Space Complexity : O(1) as we do not use any extra space
+
+Video Explanation : https://drive.google.com/file/d/1efcM5ICkwmQLzFpC8siA7aRKKY_-m_vy/view?usp=sharing
+
+*/
